34ed98a48e4ff6a7d0ac07ab7e2221c36340aa05
qqwtxxg <144107505+qqwtxxg@users.noreply.github.com>
2023-12-07 15:01:51 +0300

    Add files via upload

---
+++ domashnyaya_kr1_pascal.pas
@@ -0,0 +1,24 @@
+﻿Program cr1;
+var x,y,a,b: real;
+begin
+write('Впишите x: ');
+readln(x);
+if x<-7 then 
+  y:= 41/cos(x) + cos(2*x)
+else if x>-3 then
+  y:=(power(x,0.1*x)*power(x,0.1*x)-exp(x));
+writeln('Функция: ',y);
+writeln();
+x:=-9;
+
+while x<=-1 do
+begin
+  if x<-7 then 
+  y:= 41/cos(x) + cos(2*x)
+else if x>-3 then
+  y:=(power(x,0.1*x)*power(x,0.1*x)-exp(x));
+writeln('Функция: ',y);
+x:=x+0.3;
+end;
+
+end.
